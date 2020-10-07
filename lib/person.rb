6fbb03c04b58f7fe1d19b15b070101476d59d652
ChrisDemahy <demahyc@gmail.com>
2020-10-07 11:30:15 -0500

    Finished

--- lib/person.rb
+++ lib/person.rb
@@ -59,7 +59,22 @@ class Person
     def call_friend(person)
         self.happiness += 3
         person.happiness += 3
+        "Hi #{person.name}! It's #{self.name}. How are you?"
     end
 
+    def start_conversation(person, topic)
+      if topic == 'politics'
+        self.happiness -= 2
+        person.happiness -= 2
+        return 'blah blah partisan blah lobbyist' 
+      elsif topic == 'weather'
+        self.happiness += 1
+        person.happiness += 1
+        return 'blah blah sun blah rain'
+      elsif topic == 'programming'
+        return 'blah blah blah blah blah'
+      end
+
+    end
 
 end
